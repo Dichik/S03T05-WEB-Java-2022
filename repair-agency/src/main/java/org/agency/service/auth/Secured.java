069af8c7e86edf26819c01b7df986de27347aa0b
Dichik <omeluan.dima@gmail.com>
2022-11-07 17:19:19 +0200

    assignment #2: create annotation for making secured method executions in the future

---
+++ repair-agency/src/main/java/org/agency/service/auth/Secured.java
@@ -0,0 +1,12 @@
+package org.agency.service.auth;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Secured {
+    // TODO research AOP and add annotation for secured operations
+}
